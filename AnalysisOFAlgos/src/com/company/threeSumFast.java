7c933175b2b872122e9bb53c2b482890cd32eceb
Adriza Mishra <azirdamishra@gmail.com>
2020-05-15 13:13:12 +0530

    2020 may

--- AnalysisOFAlgos/src/com/company/threeSumFast.java
+++ AnalysisOFAlgos/src/com/company/threeSumFast.java
@@ -1,5 +1,36 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class threeSumFast {
+    //do not instantiate
+    private threeSumFast(){}
+
+    //check for duplicated integers
+    private static boolean containsDuplicateIntegers(int [] a){
+        for(int i = 0; i< a.length; i++){
+            if(a[i] == a[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int printAll(int[] a){
+//        int n = a.length;
+        Arrays.sort(a);
+        int count = 0;
+        if(containsDuplicateIntegers(a)) throw new IllegalArgumentException("array contains duplicate intergers");
+        for(int i = 0; i< a.length; i++){
+            for(int j = i+1; j< a.length; j++){
+                int k = Arrays.binarySearch(a, -(a[i] + a[j]));
+                if(k<j){
+                    System.out.println(a[i] + " " + a[j] + " " + a[k]);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
 }
