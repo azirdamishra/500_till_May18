76aa8977a750001d06e39cf78388e733d3cef77d
Adriza Mishra <azirdamishra@gmail.com>
2021-02-11 23:15:09 +0530

    i'm bacc 2

--- 500_till_May18th/src/com/company/FebLong3.java
+++ 500_till_May18th/src/com/company/FebLong3.java
@@ -1,4 +1,7 @@
 package com.company;
+
+import java.util.Scanner;
+
 /*
 For each friend, can you help Chef find out if this friend
  will be able to attend the meeting? More formally,
@@ -9,34 +12,61 @@ For each friend, can you help Chef find out if this friend
  */
 public class FebLong3 {
     public static void main(String[] args) {
-        InputReader in = new InputReader(System.in);
-        int t = in.readInt();
-        while (t-- >0){
-            String chefTime = in.readString();
-            int numFrens = in.readInt();
-            int [] frenTimings = new int[2*numFrens];
-            for (int i = 0; i < 2*numFrens; i++) {
-                String str = in.readString();
-                //str = str.substring(0,2) + str.substring(3,5);
-//                if(str.charAt(6) == 'A'){
-//                    frenTimings[i] = Integer.parseInt(str);
-//                }
-//                if(str.charAt(6) == 'P'){
-//                    frenTimings[i] = Integer.parseInt(str) + 1200;
-//                }
+        Scanner in = new Scanner(System.in);
+        int t = Integer.parseInt(in.nextLine());
+        //imp input taking: https://www.geeksforgeeks.org/why-is-scanner-skipping-nextline-after-use-of-other-next-functions/
+        while (t-- > 0){
+            String[] meetingTime = in.nextLine().split(" ");
+
+            int meetingTimeInt = convertToNum(meetingTime[0],meetingTime[1]);
+
+            int noOfFrens = Integer.parseInt(in.nextLine());
+
+            String answer = "";
+
+            while (noOfFrens-- >0){
+                String[] st = in.nextLine().split(" ");
+                int first = convertToNum(st[0], st[1]);
+                //System.out.println("This is first " + first);
+
+                int second = convertToNum(st[2], st[3]);
+                //System.out.println("This is second "+ second);
+                if(isItWithin(first, second, meetingTimeInt)){
+                    answer += "1";
+                }
+                else answer += "0";
             }
-            char AmorPm = chefTime.charAt(6);
-            chefTime = chefTime.substring(0,2) + chefTime.substring(3,5);
-            int chefTimeInt = Integer.parseInt(chefTime);
-            if(AmorPm == 'P') chefTimeInt += 1200;
-            String answerStr = "";
-            for (int i = 0; i < frenTimings.length; i+=2) {
-                if(chefTimeInt < frenTimings[i+1] && chefTimeInt > frenTimings[i]){
-                    answerStr += "1";
-                }else answerStr += "0";
-            }
-            System.out.println(answerStr);
+
+            System.out.println(answer);
 
         }
+
     }
+    private static int convertToNum(String time, String amOrpm){
+        if(time.substring(0,2).equals("12") && amOrpm.equals("AM")){
+            return Integer.parseInt("00" + time.substring(3,5));
+        }
+
+        int number = Integer.parseInt(time.substring(0,2) + time.substring(3,5));
+        if(amOrpm.equals("PM") && !time.substring(0,2).equals("12")) number += 1200;
+        return number;
+    }
+
+    private static boolean isItWithin(int start, int end, int time){
+        return time >= start && time <= end;
+    }
+
 }
+/*          Note:
+            int ni = in.readInt();
+            int mi = in.readInt();
+            System.out.println(ni);
+            System.out.println(mi);
+            Input:
+            1
+            2 3
+            Output:
+            2
+            3
+            //even computations work in the middle of the input
+             */
